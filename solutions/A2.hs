ab52734de55a44560b4a2ea930a7183899b0ec3d
Stéphane <stephane@heex.io>
2023-10-27 04:29:40 +0200

    implement A2.1

--- solutions/A2.hs
+++ solutions/A2.hs
@@ -9,40 +9,55 @@ import Data.List (intercalate)
 
 -- Q#01
 
-promptPlayer = undefined
+promptPlayer :: Player -> String
+promptPlayer p = "Player " ++ show p ++ " # enter a value: "
 
 -- Q#02
 
-_RANGE_ = undefined
+_RANGE_ :: [Int]
+_RANGE_ = [0 .. _SIZE_ - 1]
 
 -- Q#03
 
-isDigit = undefined
+isDigit :: Char -> Bool
+isDigit c = c `elem` ['0' .. '9']
 
-
-readDigit = undefined
+readDigit :: Char -> Int
+readDigit c
+  | isDigit c == True = read [c]
+  | isDigit c == False = -1
 
 -- Q#04
 
-_EMPTY_ROW_ = undefined
+_EMPTY_ROW_ = replicate _SIZE_ Void
 
 
-_EMPTY_BOARD_ = undefined
+_EMPTY_BOARD_ = replicate _SIZE_ _EMPTY_ROW_
 
 -- Q#05
 
-isTied = undefined
+isTied :: Board -> Bool
+isTied board =
+  notElem Void (concat board)
 
-
-_TIED_BOARD_ = undefined
+_TIED_BOARD_ :: Board
+_TIED_BOARD_ = [
+    [X, O, O]
+  , [O, X, X]
+  , [O, X, O]
+  ]
 
 -- Q#06
 
-indexRowStrings = undefined
+indexRowStrings :: [String] -> [(Char, String)]
+indexRowStrings stringList =
+  zip ['A'..] stringList
 
 -- Q#07
 
-formatLine = undefined
+formatLine :: [String] -> String
+formatLine stringList =
+  _SEP_ ++ (intercalate _SEP_ stringList) ++ _SEP_
 
 -- *** Assignment 2-2 *** --
 
